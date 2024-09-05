35f9722c591e28ebbcefc514b0e84c0ebe822c93
David Wang <davidlwang2004@gmail.com>
2024-09-05 17:00:27 -0400

    Finished part 1

--- assignment2/src/reimplementation.ml
+++ assignment2/src/reimplementation.ml
@@ -1,8 +1,8 @@
 (*
   FPSE Assignment 2
 
-  Name                  :
-  List of Collaborators :
+  Name                  : David Wang
+  List of Collaborators : Emma Levitsky
 
   Please make a good faith effort at listing people you discussed any problems with here, as per the course academic integrity policy.  CAs/Prof need not be listed!
 
@@ -26,21 +26,50 @@ open Core
 (* Disables "unused variable" warning from dune while you're still solving these! *)
 [@@@ocaml.warning "-27"]
 
+(*
 let unimplemented () =
   failwith "unimplemented"
+*)
 
 (*
 	Refer to Assignment 1 for the expected behavior of these functions.	 
 *)
 
-let factors (n : int) : int list =
-  unimplemented ()
+(*
+  Given a positive integer `n`, produce the list of integers in the range (0, n] which it is divisible by, in ascending order.
+*)
+let factors (n : int) : int list = match n with
+ | 0 -> []
+ | x -> List.init x ~f:(fun i -> i + 1) |> List.filter ~f:(fun m -> n mod m = 0);;
 
+(*
+  Given non-negative integers `n`, `d`, and `k`, produce the arithmetic progression [n; n + d; n + 2d; ...; n + (k-1)d].
+*)
 let arithmetic_progression (n : int) (d : int) (k : int) : int list =
-  unimplemented ()
+  List.init k ~f:(fun k -> n + (k * d));;
 
+(* Function to compare two strings *)
+let compare_strings s1 s2 =
+  String.compare s1 s2;;
+
+(*
+  Given a string and a list of strings, insert the string into the list so that if the list was originally ordered, then it remains ordered. Return as a result the list with the string inserted.
+
+  If the list is not ordered, then return `Error "insert into unordered list"`.
+
+	Note this is an example of a *functional data structure*, instead of mutating	you return a fresh copy with the element added.
+*)
 let insert_string (s : string) (ls : string list) : (string list, string) result =
-	unimplemented ()
+  if List.is_sorted ls ~compare:compare_strings then
+	  let smaller, greater = 
+      List.partition_tf ls ~f:(fun str -> String.(str < s)) in
+    Ok(smaller @ s :: greater)
+  else Error("insert into unordered list")
 
+(* 
+  Split a list `ls` into two pieces, the first of which is the first `n` elements of `ls`,
+  and the second is all remaining elements.
+  e.g. split_list [1;2;3;4;5] 3 evaluates to ([1;2;3], [4;5])	
+*)
 let split_list (ls : 'a list) (n : int) : 'a list * 'a list =
-  unimplemented ()
+  List.split_n ls n;;
